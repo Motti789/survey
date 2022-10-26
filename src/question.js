afd8ba8a1ec7ef4c9b3b647c5697cc6c0710731e
Motti789 <mchillywilly@gmail.com>
2022-10-26 11:08:38 -0400

    created the form for the questions

--- src/question.js
+++ src/question.js
@@ -3,15 +3,26 @@ class Question{
     this.question = question
   }
 
-// instance method to render the object to the dom
-
+  // instance method to render the object to the dom
   renderQuestions() {
    let questionsDiv = document.getElementById("questions-container")
     
    questionsDiv.innerHTML += `
-   <ul>
-    <li> Hey there!</li>
-   </ul>
+    <form>
+     <p><label for="w3review">Review of W3Schools:</label></p>
+     <textarea id="w3review" name="w3review" rows="4" cols="50">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea>
+     <br>
+   
+     <p><label for="w3review">Review of W3Schools:</label></p>
+     <textarea id="w3review" name="w3review" rows="4" cols="50">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea>
+     <br>
+   
+     <p><label for="w3review">Review of W3Schools:</label></p>
+     <textarea id="w3review" name="w3review" rows="4" cols="50">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea>
+     <br>
+     
+     <input type="submit" value="Submit">
+    </form>
    `
   }
 
